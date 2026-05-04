00cb2f97169d55db0f19e40431849bb364ece4f7
victor7w7r <arkano036@gmail.com>
2026-05-03 21:12:01 -0500

    fix: remove bananapi

--- nixos/kernel/sunxi/dts.nix
+++ nixos/kernel/sunxi/dts.nix
@@ -4,7 +4,5 @@
   cp -r ${armbian}/patch/kernel/archive/sunxi-6.18/dt_64/sun50i-h618-bananapi-m4-berry.dts arch/arm64/boot/dts/allwinner/
   cp -r ${armbian}/patch/kernel/archive/sunxi-6.18/dt_64/sun50i-h618-bananapi-m4-zero.dts arch/arm64/boot/dts/allwinner/
 
-  echo "dtb-\$(CONFIG_ARCH_SUNXI) += sun50i-h618-bananapi-m4-berry.dtb" >> "arch/arm64/boot/dts/allwinner/Makefile"
-  echo "dtb-\$(CONFIG_ARCH_SUNXI) += sun50i-h618-bananapi-m4-zero.dtb" >> "arch/arm64/boot/dts/allwinner/Makefile"
   echo "subdir-y       := \$(dts-dirs) overlay" >> "arch/arm64/boot/dts/allwinner/Makefile"
 ''
