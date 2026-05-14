f047b8bc71905ceebd818bc5f25eddde18dea82d
victor7w7r <arkano036@gmail.com>
2026-05-14 09:40:48 -0500

    fix: add ip net config in kernel

--- nixos/modules/core/boot/sysctl.nix
+++ nixos/modules/core/boot/sysctl.nix
@@ -21,6 +21,8 @@
     "kernel.split_lock_mitigate" = 0;
     "kernel.sysrq" = 1;
     "kernel.unprivileged_userns_clone" = 1;
+    "net.bridge.bridge-nf-call-iptables" = 1;
+    "net.bridge.bridge-nf-call-ip6tables" = 1;
     "net.core.bpf_jit_limit" = 1000000000;
     "net.core.default_qdisc" = "cake";
     "net.core.netdev_max_backlog" = 4096;
@@ -36,12 +38,15 @@
     "net.ipv4.ip_forward" = 1;
     "net.ipv4.conf.all.accept_redirects" = 0;
     "net.ipv4.conf.all.forwarding" = 1;
-    "net.ipv4.conf.default.accept_redirects" = 0;
     "net.ipv4.conf.all.secure_redirects" = 0;
     "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.all.rp_filter" = 0;
+    "net.ipv4.conf.default.rp_filter" = 0;
+    "net.ipv4.conf.brint.rp_filter" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
     "net.ipv4.conf.default.secure_redirects" = 0;
     "net.ipv4.conf.default.send_redirects" = 0;
-    "net.ipv4.icmp_echo_ignore_all" = 1;
+    "net.ipv4.icmp_echo_ignore_all" = 0;
     "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
     "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
     "net.ipv4.tcp_congestion_control" = "bbr";
