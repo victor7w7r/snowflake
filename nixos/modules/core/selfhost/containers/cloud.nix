c2edb22a1c45cad093f4e71f6df8e692bb2404e1
victor7w7r <arkano036@gmail.com>
2026-05-14 19:30:01 -0500

    fix: return to docker again

--- nixos/modules/core/selfhost/containers/cloud.nix
+++ nixos/modules/core/selfhost/containers/cloud.nix
@@ -1,12 +1,6 @@
 { lib, ... }:
 {
-  #podman exec -it seafile python3 /scripts/start.py
-  systemd.services."container@cloud" = {
-    environment = {
-      SYSTEMD_NSPAWN_USE_CGNS = "0";
-      SYSTEMD_NSPAWN_UNIFIED_HIERARCHY = "1";
-    };
-  };
+  #docker exec -it seafile python3 /scripts/start.py
   containers.cloud = {
     autoStart = true;
     privateNetwork = true;
@@ -20,24 +14,6 @@
     additionalCapabilities = [
       ''all" --system-call-filter="add_key keyctl bpf" --capability="all''
     ];
-    allowedDevices = [
-      {
-        node = "/dev/fuse";
-        modifier = "rwm";
-      }
-      {
-        node = "/dev/mapper/control";
-        modifier = "rwm";
-      }
-      {
-        node = "/dev/console";
-        modifier = "rwm";
-      }
-      {
-        node = "/dev/kmsg";
-        modifier = "rwm";
-      }
-    ];
 
     bindMounts = {
       "/opt/seafile-mysql/db" = {
@@ -48,20 +24,10 @@
         hostPath = "/nix/persist/cloud/seafile/shared";
         isReadOnly = false;
       };
-      kmsg = {
-        hostPath = "/dev/kmsg";
-        mountPoint = "/dev/kmsg";
-        isReadOnly = false;
-      };
-      fuse = {
-        hostPath = "/dev/fuse";
-        mountPoint = "/dev/fuse";
-        isReadOnly = false;
-      };
     };
 
     config =
-      { pkgs, ... }:
+      { ... }:
       {
         system.stateVersion = "26.05";
         boot.isContainer = true;
@@ -73,27 +39,19 @@
             "8.8.8.8"
           ];
         };
-        environment.systemPackages = with pkgs; [
-          fuse-overlayfs
-          util-linux
-          dbus
-        ];
         services = {
           resolved.enable = true;
           journald.extraConfig = "SystemMaxUse=100M";
         };
-        systemd.tmpfiles.rules = [ "d /opt/seafile-data 0770 1000 1000 - -" ];
 
-        virtualisation.podman = {
-          enable = true;
-          extraPackages = with pkgs; [
-            fuse-overlayfs
-          ];
-        };
+        systemd.tmpfiles.rules = [
+          "d /opt/seafile-data 0770 1000 1000 - -"
+        ];
 
+        virtualisation.docker.enable = true;
         virtualisation.oci-containers.containers = {
           "seafile-mysql" = {
-            image = "docker.io/mariadb:10.11";
+            image = "mariadb:10.11";
             environment = {
               MYSQL_ROOT_PASSWORD = "db_dev";
               MYSQL_LOG_CONSOLE = "true";
@@ -104,7 +62,7 @@
           };
 
           "seafile-memcached" = {
-            image = "docker.io/memcached:1.6.18";
+            image = "memcached:1.6.18";
             cmd = [
               "memcached"
               "-m"
@@ -116,7 +74,7 @@
           };
 
           "seafile" = {
-            image = "docker.io/seafileltd/seafile-mc:11.0-latest";
+            image = "seafileltd/seafile-mc:11.0-latest";
             autoStart = true;
             extraOptions = [
               "--network=host"
