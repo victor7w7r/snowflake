9a436201aa24d9fb1ee0eade74076324601397dd
victor7w7r <arkano036@gmail.com>
2026-05-14 20:29:01 -0500

    fix: add proxy only host

--- nixos/modules/core/selfhost/proxy.nix
+++ nixos/modules/core/selfhost/proxy.nix
@@ -55,7 +55,7 @@
           "/" = {
             proxyPass = "http://192.168.100.2:80";
             extraConfig = ''
-              proxy_set_header Host $http_host;
+              proxy_set_header Host $host;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-Proto $scheme;
