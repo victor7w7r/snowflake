8d5de62eee89ef82c851e95953d966988cbed5e2
victor7w7r <arkano036@gmail.com>
2026-05-15 19:25:15 -0500

    fix: remove patches

--- nixos/kernel/sdm845/default.nix
+++ nixos/kernel/sdm845/default.nix
@@ -19,21 +19,22 @@ let
       depmod -b "$out" -F "$buildRoot/System.map" "${configure.version}"
     '';
   */
-  patches = configure.passthru.patches;
-  kconfigFile = pkgs.writeText "kconfig-mobile" (
-    lib.concatStringsSep "\n" (
-      lib.mapAttrsToList (name: value: "${name}=${value}") (import ./config.aarch64-linux.nix)
-    )
-  );
+  /*
+    patches = configure.passthru.patches;
+    kconfigFile = pkgs.writeText "kconfig-mobile" (
+      lib.concatStringsSep "\n" (
+        lib.mapAttrsToList (name: value: "${name}=${value}") (import ./config.aarch64-linux.nix)
+      )
+    );
+  */
   build =
     (pkgs.mobile-nixos.kernel-builder {
-      inherit (configure) patches src;
+      inherit (configure) src;
       configfile = ./sdm845.config;
       isModular = false;
       isCompressed = "gz";
-
-      version = "${configure.version}${configure.passthru.localVer}";
-      modDirVersion = "${configure.version}${configure.passthru.localVer}";
+      #version = "${configure.version}${configure.passthru.localVer}";
+      #modDirVersion = "${configure.version}${configure.passthru.localVer}";
       makeImageDtbWith = "qcom/sdm845-oneplus-fajita.dtb";
     })
 
