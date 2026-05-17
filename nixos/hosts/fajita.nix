77a022d2b3219c3f24e43f0ffd0d70b101a97ad0
victor7w7r <arkano036@gmail.com>
2026-05-17 11:32:46 -0500

    fix: change lua5_4

--- nixos/hosts/fajita.nix
+++ nixos/hosts/fajita.nix
@@ -20,9 +20,9 @@ in
       libinput = prev.libinput.overrideAttrs (oldAttrs: {
         nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
           final.pkg-config
-          final.lua
+          final.lua5_4
         ];
-        buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ final.lua ];
+        buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ final.lua5_4 ];
       });
     })
   ];
