610c23ec03491cefac80be44fc5bb7dacba68c83
victor7w7r <arkano036@gmail.com>
2026-05-17 14:45:49 -0500

    fix: disable mobile desktop

--- nixos/modules/core/desktop/default.nix
+++ nixos/modules/core/desktop/default.nix
@@ -6,8 +6,10 @@
   ]
   ++ (if (host == "v7w7r-youyeetoox1") then [ (import ./xfce.nix) ] else [ ])
   ++ (if (host == "v7w7r-rc71l" || host == "v7w7r-macmini81") then [ (import ./plasma.nix) ] else [ ])
-  ++ (
+  /*
+    ++ (
     if (host == "v7w7r-fajita") then [ (import ./mobile.nix) ] else [ (import ./display-manager.nix) ]
-  )
+    )
+  */
   ++ (if (host == "v7w7r-macmini81" || host == "v7w7r-rc71l") then [ (import ./xr.nix) ] else [ ]);
 }
