c20c3958bb91c6175f8b04af05866c5325d8ae1a
victor7w7r <arkano036@gmail.com>
2026-05-17 21:42:33 -0500

    fix: add general override

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -256,7 +256,6 @@
         #nix build -L ".#nixosConfigurations.fajita.config.system.build.toplevel"
         #nix build -L ".#nixosConfigurations.fajita.config.system.build.tarball"
         #nix build -L ".#nixosConfigurations.fajita.config.mobile.outputs.android.android-bootimg"
-        #nix build -L ".#nixosConfigurations.fajita.config.mobile.outputs.generatedFilesystems.rootfs"
         #mount /dev/sde17 /mnt && rm -rf /mnt/* && tar --zstd -xvf efi.tar.zst -C /mnt/ --no-same-owner && umount /dev/sde17
         #export OPTS="noatime,compress_chksum,compress_algorithm=zstd,age_extent_cache,compress_extension=so,inline_xattr,inline_data,inline_dentry,errors=remount-ro,compress_extension=bin,atgc,flush_merge,discard,checkpoint_merge,gc_merge"
         #mount -o $OPTS /dev/sde18 /mnt && rm -rf /mnt/* && tar --zstd -xvf store.tar.zst -C /mnt/
--- nixos/hosts/fajita.nix
+++ nixos/hosts/fajita.nix
@@ -16,15 +16,17 @@ in
       makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
     })
     (final: prev: {
-      kdePackages = prev.kdePackages // {
-        plasma-workspace = prev.kdePackages.plasma-workspace.overrideAttrs (oldAttrs: {
-          cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
-            "-DGLIBC_LOCALE_GEN=OFF"
-            "-DUBUNTU_PACKAGEKIT=OFF"
-            "-DGLIBC_LOCALE_PREGENERATED=ON"
-          ];
-        });
-      };
+      kdePackages = prev.kdePackages.override (oldArgs: {
+        overrides = kfinal: kprev: {
+          plasma-workspace = kprev.plasma-workspace.overrideAttrs (oldAttrs: {
+            cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+              "-DGLIBC_LOCALE_GEN=OFF"
+              "-DUBUNTU_PACKAGEKIT=OFF"
+              "-DGLIBC_LOCALE_PREGENERATED=ON"
+            ];
+          });
+        };
+      });
     })
     (final: prev: {
       libinput = prev.libinput.overrideAttrs (oldAttrs: {
