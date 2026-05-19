79a2d173b1cf2a008905242a3ccf3235fdeefcb4
victor7w7r <arkano036@gmail.com>
2026-05-18 19:30:00 -0500

    fix: readapt qcom mobile

--- nixos/hosts/lib/qcom-845.nix
+++ nixos/hosts/lib/qcom-845.nix
@@ -14,8 +14,9 @@
 
   services.udev.extraRules = ''
     SUBSYSTEM=="input", KERNEL=="event*", ENV{ID_INPUT}=="1", SUBSYSTEMS=="input", ATTRS{name}=="pmi8998_haptics", TAG+="uaccess", ENV{FEEDBACKD_TYPE}="vibra"
-    SUBSYSTEM=="misc", KERNEL=="fastrpc-*", ENV{ACCEL_MOUNT_MATRIX}+="-1, 0, 0; 0, -1, 0; 0, 0, -1"
   '';
+
+  #SUBSYSTEM=="misc", KERNEL=="fastrpc-*", ENV{ACCEL_MOUNT_MATRIX}+="-1, 0, 0; 0, -1, 0; 0, 0, -1"
 
   environment.systemPackages = [ (pkgs.callPackage ../custom/sdm845-alsa.nix { }) ];
   hardware.enableRedistributableFirmware = true;
@@ -23,23 +24,38 @@
 
   mobile.quirks.qualcomm.sdm845-modem.enable = true;
   mobile.quirks.audio.alsa-ucm-meld = true;
-  mobile.kernel.structuredConfig = [
-    (
-      helpers: with helpers; {
-        BRIDGE = yes;
-        BRIDGE_NETFILTER = yes;
-      }
-    )
-  ];
-  mobile.usb = {
-    mode = "gadgetfs";
-    idVendor = lib.mkDefault "18D1";
-    idProduct = lib.mkDefault "D001";
-    gadgetfs.functions = {
-      adb = "ffs.adb";
-      mass_storage = "mass_storage.0";
-      rndis = "rndis.usb0";
-    };
+
+  mobile.hardware = {
+    soc = "qualcomm-sdm845";
+    ram = 8192;
+    screen.width = 1080;
+  };
+
+  mobile.device.identity.manufacturer = "OnePlus";
+  mobile.device.firmware =
+    pkgs.callPackage "${inputs.mobile-nixos}/devices/oneplus-enchilada/firmware"
+      { };
+
+  mobile.boot.stage-1 = {
+    compression = "xz";
+    kernel.package = (pkgs.callPackage ../../kernel/sdm845 { inherit kernelData; }).build;
+    firmware = [
+      (pkgs.runCommand "initrd-firmware" { } ''
+        cp -vrf ${config.mobile.device.firmware} $out
+        chmod -R +w $out
+        find $out/lib/firmware/qcom/sdm845 -name "modem.mbn" -delete -print
+        cp -vf ${pkgs.linux-firmware}/lib/firmware/qcom/{a630_sqe.fw,a630_gmu.bin} $out/lib/firmware/qcom
+      '')
+    ];
+  };
+
+  mobile.usb.mode = "gadgetfs";
+  mobile.usb.idVendor = lib.mkDefault "18D1";
+  mobile.usb.idProduct = lib.mkDefault "D001";
+  mobile.usb.gadgetfs.functions = {
+    adb = "ffs.adb";
+    mass_storage = "mass_storage.0";
+    rndis = "rndis.usb0";
   };
 
   mobile.system.android = {
@@ -55,32 +71,6 @@
     appendDTB = lib.mkDefault [
       "dtbs/qcom/sdm845-${config.mobile.device.name}.dtb"
     ];
-  };
-
-  mobile = {
-    hardware = {
-      soc = "qualcomm-sdm845";
-      ram = 8192;
-      screen.width = 1080;
-    };
-
-    boot.stage-1 = {
-      compression = "xz";
-      kernel.package = (pkgs.callPackage ../../kernel/sdm845 { inherit kernelData; }).build;
-      firmware = [
-        (pkgs.runCommand "initrd-firmware" { } ''
-          cp -vrf ${config.mobile.device.firmware} $out
-          chmod -R +w $out
-          find $out/lib/firmware/qcom/sdm845 -name "modem.mbn" -delete -print
-          cp -vf ${pkgs.linux-firmware}/lib/firmware/qcom/{a630_sqe.fw,a630_gmu.bin} $out/lib/firmware/qcom
-        '')
-      ];
-    };
-
-    device = {
-      identity.manufacturer = "OnePlus";
-      firmware = pkgs.callPackage "${inputs.mobile-nixos}/devices/oneplus-enchilada/firmware" { };
-    };
   };
 
   /*
