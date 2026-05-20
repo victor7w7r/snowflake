5c8aaac8f17029598cc3a80599f7754606cd4e70
victor7w7r <arkano036@gmail.com>
2026-05-20 09:49:24 -0500

    fix: move dendritic

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
+    agenix.url = "github:ryantm/agenix";
     batfetch = {
       url = "github:ashish-kus/batfetch";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin-refind = {
+      url = "github:catppuccin/refind";
+      flake = false;
     };
     claude-desktop = {
       url = "github:k3d3/claude-desktop-linux-flake";
@@ -17,8 +22,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     den.url = "github:denful/den";
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-file.url = "github:vic/flake-file";
@@ -26,15 +35,38 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    gestures.url = "github:ferstar/gestures";
+    hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland.url = "https://flakehub.com/f/hyprwm/Hyprland/0.53";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+    impermanence.url = "github:nix-community/impermanence";
     import-tree.url = "github:vic/import-tree";
+    kwin-effects-better-blur-dx = {
+      url = "github:xarblu/kwin-effects-better-blur-dx";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mobile-nixos = {
+      url = "github:mobile-nixos/mobile-nixos";
+      flake = false;
+    };
+    nix-alien.url = "https://flakehub.com/f/thiagokokada/nix-alien/0.1";
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     nix-doom-emacs-unstraightened = {
       url = "github:marienz/nix-doom-emacs-unstraightened";
       inputs.nixpkgs.follows = "";
     };
+    nix-flatpak.url = "https://flakehub.com/f/gmodena/nix-flatpak/0.7.0";
+    nix-gaming.url = "github:fufexan/nix-gaming";
+    nix-search-tv.url = "github:3timeslazy/nix-search-tv";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixos-wsl = {
       url = "github:nix-community/nixos-wsl";
       inputs = {
@@ -43,11 +75,28 @@
       };
     };
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";
+    pyprland.url = "github:hyprland-community/pyprland";
     swiftfetch = {
       url = "github:ly-sec/swiftfetch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     thorium.url = "github:almahdi/nix-thorium";
+    xrlinux = {
+      url = "github:wheaney/XRLinuxDriver";
+      flake = false;
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/inputs/apps.nix
+++ modules/inputs/apps.nix
@@ -1,12 +1,11 @@
+/*
+  firefox-addons = {
+  url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+  inputs.nixpkgs.follows = "nixpkgs";
+  };
+*/
 {
   flake-file.inputs = {
-    /*
-      firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-      };
-    */
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
