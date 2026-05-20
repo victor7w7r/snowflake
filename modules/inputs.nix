c0b8a2e4861beb9b4fda14bc7c2f70b828a763c9
victor7w7r <arkano036@gmail.com>
2026-05-20 08:05:41 -0500

    feat: setup migration

---
+++ modules/inputs.nix
@@ -0,0 +1,20 @@
+#nix run .#write-flake
+{
+  flake-file.inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-wsl = {
+      url = "github:nix-community/nixos-wsl";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "";
+    };
+  };
+}
