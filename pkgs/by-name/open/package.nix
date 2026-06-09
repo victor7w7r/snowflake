041325144e8afa8cd52257720ea9b7687f274720
victor7w7r <arkano036@gmail.com>
2026-06-08 20:45:07 -0500

    fix: release open package

---
+++ pkgs/by-name/open/package.nix
@@ -0,0 +1,16 @@
+{ fetchurl, stdenvNoCC }:
+stdenvNoCC.mkDerivation rec {
+  pname = "open";
+  version = "0.0.3";
+
+  src = fetchurl {
+    url = "https://github.com/witt-bit/pc-guide/releases/download/v${version}/open.sh";
+    sha256 = "d9d0ae0225817945f4c8cf8a37741ce884226c6b8f66066a557f122c4d3c2305";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/open
+    chmod +x $out/bin/open
+  '';
+}
