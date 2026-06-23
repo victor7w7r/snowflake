bb963075256509350057ba49f496ffb4399800ba
victor7w7r <arkano036@gmail.com>
2026-06-22 21:52:54 -0500

    fix: finish tewi

--- pkgs/by-name/tewi/package.nix
+++ pkgs/by-name/tewi/package.nix
@@ -1,4 +1,22 @@
 { pkgs, fetchFromGitHub }:
+let
+  geoip2fast = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "geoip2fast";
+    version = "1.2.2";
+    pyproject = true;
+
+    src = pkgs.python3.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-OIFXAM7f6xl9UbS4czsNT3lls23hUUfBJVJxJPi0XWs=";
+    };
+
+    nativeBuildInputs = with pkgs.python3Packages; [
+      setuptools
+      wheel
+    ];
+    doCheck = false;
+  };
+in
 pkgs.python3.pkgs.buildPythonApplication (attrs: {
   pname = "tewi";
   version = "master";
@@ -8,10 +26,16 @@ pkgs.python3.pkgs.buildPythonApplication (attrs: {
     owner = "anlar";
     repo = attrs.pname;
     rev = attrs.version;
-    sha256 = "sha256-Twhr9+gGb+3d91JAAACUCPtSdOfcLZAq4jvH6llQ9no=";
+    sha256 = "sha256-FDzZzUAlgmKP5dhL9SsAhKY/z/PYu/4uE9hEmYqd9i4=";
   };
 
   propagatedBuildInputs = with pkgs.python3Packages; [
+    textual
+    transmission-rpc
+    geoip2fast
+    pyperclip
+    qbittorrent-api
+    platformdirs
   ];
 
   build-system = with pkgs.python3.pkgs; [
