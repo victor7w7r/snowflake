577f2e5e4faf7bdc1cb14e69449d277658f87aeb
victor7w7r <arkano036@gmail.com>
2026-06-23 18:33:48 -0500

    fix: segregate autoricer lock

--- pkgs/by-name/autoricer/package.nix
+++ pkgs/by-name/autoricer/package.nix
@@ -7,8 +7,14 @@ rustPlatform.buildRustPackage (attrs: {
     owner = "3rfaan";
     repo = attrs.pname;
     rev = attrs.version;
-    sha256 = "sha256-4jwKxsxIKpeelrruhFiu3o8AAxMp+CTNuSXK7XBiXFU=";
+    sha256 = "sha256-8Ff9QhdQ1BrpKN2r81gAGlZGaybCdI2pIl3PQVUaSbU=";
   };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  prePatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+  '';
 
   cargoHash = "sha256-ywqXUp3X9JfAAAOdWyyrUPaAJx+I3cvPQU+7nP2okpM=";
 })
