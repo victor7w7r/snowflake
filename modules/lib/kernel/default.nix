d7ab2968fbab986264eedf75c15aa03677d24965
victor7w7r <arkano036@gmail.com>
2026-06-24 18:36:46 -0500

    fix: put handheld outside

--- modules/lib/kernel/default.nix
+++ modules/lib/kernel/default.nix
@@ -3,7 +3,9 @@
   imports = [ (inputs.den.namespace "kernel" true) ];
 
   kernel = {
-    handheld.nixos.nixpkgs.overlays = [ (_: prev: kernel.hosts.handheld prev) ];
+    handheld = {
+      nixos.nixpkgs.overlays = [ (_: prev: kernel.hosts.handheld prev) ];
+    };
     main.nixos.nixpkgs.overlays = [ (_: prev: kernel.hosts.main prev) ];
     server.nixos.nixpkgs.overlays = [ (_: prev: kernel.hosts.server prev) ];
     pizero.nixos.nixpkgs.overlays = [ (_: prev: kernel.hosts.pizero prev) ];
