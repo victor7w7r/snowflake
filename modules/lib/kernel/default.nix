4fca1ba74b71dbe97abc6724d36377d295609526
victor7w7r <arkano036@gmail.com>
2026-06-24 21:42:17 -0500

    fix: set kernel to debug in trace for shell

--- modules/lib/kernel/default.nix
+++ modules/lib/kernel/default.nix
@@ -3,11 +3,23 @@
   imports = [ (inputs.den.namespace "kernel" true) ];
 
   kernel = {
-    handheld.nixos.nixpkgs.overlays = [ (final: _: kernel.hosts.handheld final) ];
+    handheld.nixos.nixpkgs.overlays = [
+      (final: _: kernel.hosts.handheld final)
+      (final: _: builtins.trace (kernel.hosts.attrTest final) (kernel.hosts.attrTest final))
+    ];
     main.nixos.nixpkgs.overlays = [ (final: _: kernel.hosts.main final) ];
     server.nixos.nixpkgs.overlays = [ (final: _: kernel.hosts.server final) ];
     pizero.nixos.nixpkgs.overlays = [ (final: _: kernel.hosts.pizero final) ];
     superlab.nixos.nixpkgs.overlays = [ (final: _: kernel.hosts.superlab final) ];
+
+      hosts.test.attrTest = final: final.writeShellApplication {
+        name = "hola-test";
+        text = ''
+          echo "¡La configuración de overlays está andando, ctm!"
+          echo "Kernel actual: $(uname -r)"
+        '';
+      };
+    };
 
     lib = {
       injector = pkgs: {
