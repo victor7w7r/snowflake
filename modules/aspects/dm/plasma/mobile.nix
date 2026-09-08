5de6e88277386969dfa89907eae2c19a2f3cdda4
victor7w7r <arkano036@gmail.com>
2026-09-08 15:51:51 -0500

    move persistence in environment

--- modules/aspects/dm/plasma/mobile.nix
+++ modules/aspects/dm/plasma/mobile.nix
@@ -9,13 +9,15 @@
       }:
       lib.optionalAttrs isPhone {
         services.xserver.enable = true;
-        persistence."/nix/persist".users."${user.name}".files = [ ".config/plasmamobilerc" ];
-        environment.systemPackages = with pkgs.kdePackages; [
-          plasma-mobile
-          plasma-nano
-          plasma-dialer
-          spacebar
-        ];
+        environment = {
+          persistence."/nix/persist".users."${user.name}".files = [ ".config/plasmamobilerc" ];
+          systemPackages = with pkgs.kdePackages; [
+            plasma-mobile
+            plasma-nano
+            plasma-dialer
+            spacebar
+          ];
+        };
       };
   };
 }
