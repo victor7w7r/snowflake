5df04b15a83fccc7d4f8d55117fa0633a10e885d
victor7w7r <arkano036@gmail.com>
2026-09-08 07:42:21 -0500

    remove xiaomi mi mix3

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -27,40 +27,7 @@
           |> builtins.filter (
             item:
             !builtins.elem item.name [
-             /* "0001-arm64-dts-qcom-sdm845-xiaomi-beryllium-Enable-ath10k"
-              "0027-arm64-dts-qcom-sdm670-google-sargo-Enable-fuel-gauge"
-              "0079-arm64-dts-qcom-sdm845-shift-axolotl-Introduce-camera"
-              "0088-arm64-dts-qcom-sdm845-shift-axolotl-Enable-NFC"
-              "0089-arm64-dts-qcom-sdm845-google-common-Enable-NFC"
-              "0092-Input-synaptics-rmi4-handle-duplicate-unknown-PDT-en"
-              "0093-Input-synaptics-rmi4-f12-use-hardcoded-values-for-af"
-              "0097-Input-synaptics-rmi4-support-fallback-values-for-PDT"
-              "0107-arm64-dts-qcom-Introduce-support-for-Xiaomi-Mi-Mix-3"
-              "0115-arm64-dts-qcom-sdm845-xiaomi-beryllium-Introduce-fra"
-              "0117-arm64-dts-qcom-sdm845-shift-axolotl-Convert-fb-to-us"
-              "0118-arm64-dts-qcom-sdm845-samsung-starqltechn-Convert-fb"
-              "0123-Input-stmfts-Fix-the-MODULE_LICENSE-string"
-              "0124-Input-stmfts-Use-dev-struct-directly"
-              "0125-Input-stmfts-Switch-to-devm_regulator_bulk_get_const"
-              "0126-Input-stmfts-abstract-reading-information-from-the-f"
-              "0127-Input-stmfts-disable-regulators-when-power-on-fails"
-              "0128-Input-stmfts-use-client-to-make-future-code-cleaner"
-              "0129-dt-bindings-input-touchscreen-st-stmfts-Introduce-re"
-              "0130-Input-stmfts-add-optional-reset-GPIO-support"
-              "0132-Input-stmfts-support-FTS5"
-              "0141-arm64-dts-qcom-sdm845-shift-axolotl-describe-WiFi-BT"
-              "0148-dt-bindings-arm-qcom-Add-Xiaomi-Poco-F1-Tianma-varia"
-              "0149-arm64-dts-qcom-sdm845-xiaomi-beryllium-Fix-compatibl"
-              "0153-media-qcom-camss-Prepare-CSID-for-C-PHY-support"
-              "0154-media-qcom-camss-Initialize-lanes-after-lane-configu"
-              "0155-media-qcom-camss-csiphy-3ph-Add-Gen2-v1.1-MIPI-CSI-2"
-              "0156-media-qcom-camss-csiphy-3ph-Add-Gen2-v1.2.1-MIPI-CSI"
-              "0162-arm64-dts-qcom-sdm845-shift-axolotl-Add-actuator-for"
-              "0185-arm64-dts-qcom-sdm845-lg-common-Add-camera-flash"
-              "0186-arm64-dts-qcom-sdm845-lg-common-Change-ipa-gsi-loade"
-              "0187-arm64-dts-qcom-sdm845-lg-judyln-judyp-Reference-memo"
-              "0188-arm64-dts-qcom-sdm845-lg-Enable-qcom-snoc-host-cap-s"
-              "0194-sdm845.config-further-cleanup-and-some-moved-into-mi"*/
+            	"0107-arm64-dts-qcom-Introduce-support-for-Xiaomi-Mi-Mix-3"
             ]
           )
           |> map (item: "${patches}/${item.name}.patch")
