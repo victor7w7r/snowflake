1dd064f6b672d96ac1410688061494a606847789
victor7w7r <arkano036@gmail.com>
2026-09-09 16:34:14 -0500

    exclude 0186-arm64-dts-qcom-sdm845-google-Add-WCD9340-codec-and-m.patch

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -86,6 +86,7 @@
           "0176-EDITME-cover-title-for-wcd-dma.patch"
           "0178-Speakers-for-Pixel-3-3-XL.patch"
           "0184-arm64-dts-qcom-sdm845-google-Add-basic-audio-support.patch"
+          "0186-arm64-dts-qcom-sdm845-google-Add-WCD9340-codec-and-m.patch"
           "0187-Pixel-3-display-fixes.patch"
           "0190-Add-modemsmem-for-Google-phones.patch"
           "0194-EDITME-cover-title-for-ea8074.patch"
