51f7cf74ff836395d65bef3d72e0b370d8342ef7
victor7w7r <arkano036@gmail.com>
2026-09-09 15:16:01 -0500

    exclude 0059-hack-arm64-dts-xiaomi-beryllium-common-Add-nodes-for.patch

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -27,6 +27,7 @@
           "0046-hack-ASoC-qdsp6-q6afe-dai-add-internal-mi2s-support.patch" # !
           "0047-hack-ASoC-qdsp6-q6routing-add-internal-mi2s-support.patch" # !
           "0058-hack-arm64-dts-qcom-sdm845-Add-q6voice-APR-service-d.patch"
+          "0059-hack-arm64-dts-xiaomi-beryllium-common-Add-nodes-for.patch"
           "0062-END-HACK-series-for-working-qdsp6.patch"
           "0065-Enable-DW9714V-camera-focus-coils-on-SHIFT-6MQ.patch"
           "0069-EDITME-cover-title-for-shift6-camera.patch"
