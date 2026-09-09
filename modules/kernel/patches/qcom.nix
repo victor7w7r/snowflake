9f8555ea85273eb3800d29b995cbff94ee765fef
victor7w7r <arkano036@gmail.com>
2026-09-09 14:34:27 -0500

    exclude 0012-arm64-dts-qcom-sdm845-oneplus-Add-haptics-support.patch

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -12,6 +12,7 @@
           "0004-Xiaomi-Poco-F1-ebbg-display-variant-fixes.patch"
           "0008-SDM845-DOWNSTREAM-EDITME-cover-title-for-qcom-spmi-s.patch"
           "0011-arm64-dts-qcom-pmi8998-Introduce-SPMI-haptics.patch"
+          "0012-arm64-dts-qcom-sdm845-oneplus-Add-haptics-support.patch"
         ]
       )
       |> map (filename: "${self}/modules/kernel/patches/sdm845/${filename}");
