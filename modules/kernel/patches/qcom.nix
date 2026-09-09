c765087da3a02b020209d6dd250288e02626bf9a
victor7w7r <arkano036@gmail.com>
2026-09-09 15:31:09 -0500

    exclude 0087-arm64-dts-qcom-sdm845-google-Add-dual-front-IMX355-c.patch

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -43,6 +43,7 @@
           "0084-arm64-dts-renesas-Convert-to-new-media-orientation-d.patch"
           "0085-arm64-dts-rockchip-Convert-to-new-media-orientation-.patch"
           "0086-Add-initial-dual-front-camera-and-rear-flash-support.patch"
+          "0087-arm64-dts-qcom-sdm845-google-Add-dual-front-IMX355-c.patch"
           "0089-EDITME-downstream-cameras.patch"
           "0100-power-supply-pmi8998-charger-improvements-and-smb5-s.patch"
           "0106-xiaomi-perseus-support.patch"
