eb3d01b5aa85d3a8855f2570887acf8617e81f39
victor7w7r <arkano036@gmail.com>
2026-09-09 15:10:05 -0500

    exclude /0047-hack-ASoC-qdsp6-q6routing-add-internal-mi2s-support

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -25,6 +25,7 @@
           "0044-hack-ASoC-qdsp6-q6dsp-lpass-ports-add-internal-mi2s-.patch" # !
           "0045-hack-ASoC-qdsp6-q6afe-add-internal-mi2s-support.patch" # !
           "0046-hack-ASoC-qdsp6-q6afe-dai-add-internal-mi2s-support.patch" # !
+          "0047-hack-ASoC-qdsp6-q6routing-add-internal-mi2s-support.patch" # !
           "0062-END-HACK-series-for-working-qdsp6.patch"
           "0065-Enable-DW9714V-camera-focus-coils-on-SHIFT-6MQ.patch"
           "0069-EDITME-cover-title-for-shift6-camera.patch"
