41726ec47517dd7b2dd6891024f38923de2bee62
victor7w7r <arkano036@gmail.com>
2026-09-09 18:38:07 -0500

    remove root config move

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -106,7 +106,7 @@
       pkgs:
       (pkgs.runCommand "qcom-defconfig" { } ''
         cp ${inputs.linux-sdm845}/arch/arm64/configs/defconfig defconfig
-        cp ${inputs.linux-sdm845}/arch/arm64/configs/sdm845.config /arch/arm64/configs/sdm845.config
+        cp ${inputs.linux-sdm845}/arch/arm64/configs/sdm845.config sdm845.config
         cat defconfig sdm845.config | sed '/CONFIG_LOCALVERSION/d' > $out
       '');
   };
