3965852de4f06f68dd637b5d28aa847da00794e6
victor7w7r <arkano036@gmail.com>
2026-09-09 21:24:18 -0500

    set map outside

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -95,12 +95,11 @@
           "0211-arm64-dts-qcom-sdm845-xiaomi-dipper-Add-touchscreen.patch"
         ]
       )
-      |> (
+      |>
         (map (filename: "${self}/modules/kernel/patches/sdm845/${filename}"))
         ++ [
           "${self}/modules/kernel/patches/files/fix-qcom-smbx-init.patch"
-        ]
-      );
+        ];
 
     qcom-defconfig =
       pkgs:
