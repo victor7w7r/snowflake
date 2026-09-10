b1c11a0b428e742462f3ba529a9e251fc7f6f06d
victor7w7r <arkano036@gmail.com>
2026-09-09 20:23:31 -0500

    comment ntfs fix

--- modules/kernel/patches/qcom.nix
+++ modules/kernel/patches/qcom.nix
@@ -16,7 +16,7 @@
           "0016-Qualcomm-3rd-gen-fuel-gauge-support.patch"
           "0019-arm64-dts-qcom-pmi8998-Add-fuel-gauge.patch"
           "0024-EDITME-cover-title-for-ath10k-a-msdu.patch"
-          #"0025-wifi-ath10k-make-in-order-rx-amsdu-buffers-persisten.patch" # !
+          "0025-ntfs-fix-resource-leak-in-ntfs_new_attr_flags.patch"
           "0026-Mainlining-effort-for-TAS255x.patch"
           "0028-arm64-dts-qcom-sdm845-xiaomi-beryllium-Enable-gpi_dm.patch"
           "0029-arm64-dts-qcom-sdm845-xiaomi-beryllium-common-add-su.patch"
