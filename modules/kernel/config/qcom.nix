ee38cebd453181ab2af981c18da6051bc35448c4
victor7w7r <arkano036@gmail.com>
2026-09-10 12:39:30 -0500

    remove SM_GCC_6125

--- modules/kernel/config/qcom.nix
+++ modules/kernel/config/qcom.nix
@@ -414,7 +414,6 @@
         SM_DISPCC_8550 = no;
         SM_DISPCC_8750 = no;
         SM_DISPCC_MILOS = no;
-        SM_GCC_6125 = no;
         SM_GCC_6375 = no;
         SM_GCC_7150 = no;
         SM_GCC_8150 = no;
