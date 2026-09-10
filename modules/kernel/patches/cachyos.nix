b2a533edf18ea9cd1c19eb4228723dbaa381b3f3
victor7w7r <arkano036@gmail.com>
2026-09-10 11:52:56 -0500

    exclude /0125-smp-Disable-preemption-explicitly-in-__csd_lock_wait.patch

--- modules/kernel/patches/cachyos.nix
+++ modules/kernel/patches/cachyos.nix
@@ -81,6 +81,7 @@
               "0089-sched-fair-Change-the-default-cgroup_mode-to-concur.patch"
               "0090-sched-eevdf-Move-to-a-single-runqueue.patch"
               "0119-list-Mark-some-functions-as-__always_inline.patch"
+              "0125-smp-Disable-preemption-explicitly-in-__csd_lock_wait.patch"
             ]
           )
           |> map (filename: "${self}/modules/kernel/patches/cachyos-7.2/${filename}");
