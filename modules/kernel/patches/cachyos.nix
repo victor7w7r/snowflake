f61cfee604aacdc86d4b60686bd0db8e02cc6f9a
victor7w7r <arkano036@gmail.com>
2026-09-10 11:51:54 -0500

    exclude 0119-list-Mark-some-functions-as-__always_inline.patch

--- modules/kernel/patches/cachyos.nix
+++ modules/kernel/patches/cachyos.nix
@@ -80,6 +80,7 @@
               "0088-sched-fair-Add-cgroup_mode-tasks.patch"
               "0089-sched-fair-Change-the-default-cgroup_mode-to-concur.patch"
               "0090-sched-eevdf-Move-to-a-single-runqueue.patch"
+              "0119-list-Mark-some-functions-as-__always_inline.patch"
             ]
           )
           |> map (filename: "${self}/modules/kernel/patches/cachyos-7.2/${filename}");
