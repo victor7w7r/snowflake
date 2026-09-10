96b063e5c93e41eab7d984354ce420eb506dd4f9
victor7w7r <arkano036@gmail.com>
2026-09-10 11:45:04 -0500

    exclude 0085-sched-fair-Add-cgroup_mode-up.patch

--- modules/kernel/patches/cachyos.nix
+++ modules/kernel/patches/cachyos.nix
@@ -74,6 +74,7 @@
               "0081-ksm-add-mremap-selftests-for-ksm_rmap_walk.patch"
               "0082-sched-core-Fix-inter-class-wakeup_preempt.patch"
               "0083-sched-fair-Fix-overflow-in-update_tg_cfs_runnable.patch"
+              "0085-sched-fair-Add-cgroup_mode-up.patch"
             ]
           )
           |> map (filename: "${self}/modules/kernel/patches/cachyos-7.2/${filename}");
