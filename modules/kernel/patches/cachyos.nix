d1791e73f7b93d92455f1e852aea174d946b44ed
victor7w7r <arkano036@gmail.com>
2026-09-10 11:48:53 -0500

    exclude 0089-sched-fair-Change-the-default-cgroup_mode-to-concur.patch

--- modules/kernel/patches/cachyos.nix
+++ modules/kernel/patches/cachyos.nix
@@ -78,6 +78,7 @@
               "0086-sched-fair-Add-cgroup_mode-max.patch"
               "0087-sched-fair-Add-cgroup_mode-concur.patch"
               "0088-sched-fair-Add-cgroup_mode-tasks.patch"
+              "0089-sched-fair-Change-the-default-cgroup_mode-to-concur.patch"
             ]
           )
           |> map (filename: "${self}/modules/kernel/patches/cachyos-7.2/${filename}");
