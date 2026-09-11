e7f0b7dd6d9c13b6078a97553748dccc57ddac79
victor7w7r <arkano036@gmail.com>
2026-09-11 09:02:11 -0500

    comment plasmamobilerc

--- modules/aspects/dm/plasma/mobile.nix
+++ modules/aspects/dm/plasma/mobile.nix
@@ -13,7 +13,7 @@
         environment = {
           persistence."/nix/persist".users."${user.name}" = {
             directories = [ ".config/plasma-mobile" ];
-            files = [ ".config/plasmamobilerc" ];
+            #files = [ ".config/plasmamobilerc" ];
           };
           plasma6.excludePackages = with pkgs.kdePackages; [
           	kcalc
