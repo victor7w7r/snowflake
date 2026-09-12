e6061c757558f49a5b1ea2262a1c82dcb52fe4bd
victor7w7r <arkano036@gmail.com>
2026-09-12 12:29:46 -0500

    change plasmamobile to ini

--- modules/aspects/dm/plasma/mobile.nix
+++ modules/aspects/dm/plasma/mobile.nix
@@ -51,48 +51,53 @@
         ...
       }:
       lib.optionalAttrs isPhone {
-        xdg.configFile."plasmamobilerc".text = lib.generators.toGitINI {
-          General = {
-            actionDrawerTopLeftMode = "0";
-            actionDrawerTopRightMode = "1";
-            dateInStatusBar = "false";
-            gesturePanelEnabled = "false";
-            showBatteryPercentage = "true";
-            statusBarScaleFactor = "1";
-            vibrationDuration = "10";
-            animationsEnabled = "false";
-            navigationPanelEnabled = "false";
-          };
-          InitialStart.wizardRun = "true";
-          QuickSettings = {
-            quickSettingsColumns = "3";
-            disabledQuickSettings = builtins.concatStringsSep "," [
-              "org.kde.plasma.quicksetting.nightcolor"
-              "org.kde.plasma.quicksetting.kscreenosd"
-              "org.kde.plasma.quicksetting.settingsapp"
-              "org.kde.plasma.quicksetting.battery"
-              "org.kde.plasma.quicksetting.powermenu"
-            ];
-            enabledQuickSettings = builtins.concatStringsSep "," [
-              "org.kde.plasma.quicksetting.wifi"
-              "org.kde.plasma.quicksetting.bluetooth"
-              "org.kde.plasma.quicksetting.mobiledata"
-              "org.kde.plasma.quicksetting.flashlight"
-              "org.kde.plasma.quicksetting.screenrotation"
-              "org.kde.plasma.quicksetting.airplanemode"
-              "org.kde.plasma.quicksetting.hotspot"
-              "org.kde.plasma.quicksetting.donotdisturb"
-              "org.kde.plasma.quicksetting.audio"
-              "org.kde.plasma.quicksetting.caffeine"
-              "org.kde.plasma.quicksetting.autohidepanels"
-              "org.kde.plasma.quicksetting.docked"
-              "org.kde.plasma.quicksetting.waydroid"
-              "org.kde.plasma.quicksetting.record"
-              "org.kde.plasma.quicksetting.screenshot"
-              "org.kde.plasma.quicksetting.keyboardtoggle"
-            ];
-          };
-        };
+        xdg.configFile."plasmamobilerc".text =
+          lib.generators.toINI
+            {
+              mkKeyValue = lib.generators.mkKeyValueDefault { } "=";
+            }
+            {
+              General = {
+                actionDrawerTopLeftMode = "0";
+                actionDrawerTopRightMode = "1";
+                dateInStatusBar = "false";
+                gesturePanelEnabled = "false";
+                showBatteryPercentage = "true";
+                statusBarScaleFactor = "1";
+                vibrationDuration = "10";
+                animationsEnabled = "false";
+                navigationPanelEnabled = "false";
+              };
+              InitialStart.wizardRun = "true";
+              QuickSettings = {
+                quickSettingsColumns = "3";
+                disabledQuickSettings = builtins.concatStringsSep "," [
+                  "org.kde.plasma.quicksetting.nightcolor"
+                  "org.kde.plasma.quicksetting.kscreenosd"
+                  "org.kde.plasma.quicksetting.settingsapp"
+                  "org.kde.plasma.quicksetting.battery"
+                  "org.kde.plasma.quicksetting.powermenu"
+                ];
+                enabledQuickSettings = builtins.concatStringsSep "," [
+                  "org.kde.plasma.quicksetting.wifi"
+                  "org.kde.plasma.quicksetting.bluetooth"
+                  "org.kde.plasma.quicksetting.mobiledata"
+                  "org.kde.plasma.quicksetting.flashlight"
+                  "org.kde.plasma.quicksetting.screenrotation"
+                  "org.kde.plasma.quicksetting.airplanemode"
+                  "org.kde.plasma.quicksetting.hotspot"
+                  "org.kde.plasma.quicksetting.donotdisturb"
+                  "org.kde.plasma.quicksetting.audio"
+                  "org.kde.plasma.quicksetting.caffeine"
+                  "org.kde.plasma.quicksetting.autohidepanels"
+                  "org.kde.plasma.quicksetting.docked"
+                  "org.kde.plasma.quicksetting.waydroid"
+                  "org.kde.plasma.quicksetting.record"
+                  "org.kde.plasma.quicksetting.screenshot"
+                  "org.kde.plasma.quicksetting.keyboardtoggle"
+                ];
+              };
+            };
       };
   };
 }
