f709d610e985692e46270c8c56d47c1b1771b730
victor7w7r <arkano036@gmail.com>
2026-09-12 09:11:05 -0500

    fix syntax for qca bluetooth

--- modules/aspects/dm/plasma/menu.nix
+++ modules/aspects/dm/plasma/menu.nix
@@ -2,7 +2,7 @@
   den.aspects.plasma.menu.provides.to-users.homeManager=
     { lib, isPhone, ... }:
     lib.optionalAttrs (!isPhone) {
-      xdgconfigFile."menus/applications-kmenuedit.menu".text = ''
+      xdg.configFile."menus/applications-kmenuedit.menu".text = ''
       <!DOCTYPE Menu PUBLIC '-//freedesktop//DTD Menu 1.0//EN' 'http://www.freedesktop.org/standards/menu-spec/1.0/menu.dtd'>
       <Menu>
         <Menu>
--- modules/hosts/phone/services/qca-bluetooth.nix
+++ modules/hosts/phone/services/qca-bluetooth.nix
@@ -1,40 +1,47 @@
 {
-  den.aspects.phone.services.qca-bluetooth.nixos = { lib, pkgs, ... }: {
-    systemd.services.qca-bluetooth = {
-      description = "Setup the bluetooth interface";
-      wantedBy = [
-        "multi-user.target"
-        "bluetooth.service"
-      ];
-      script = toString (
-        pkgs.writeShellScript "qca-bluetooth.sh" ''
-          set -x
-          trap 'sleep 1' DEBUG
-          export PATH="${
-            lib.makeBinPath (
-              with pkgs;
-              [
-                config.hardware.bluetooth.package
-                coreutils-full
-                gawk
-                unixtools.script
-              ]
-            )
-          }:$PATH"
+  den.aspects.phone.services.qca-bluetooth.nixos =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      systemd.services.qca-bluetooth = {
+        description = "Setup the bluetooth interface";
+        wantedBy = [
+          "multi-user.target"
+          "bluetooth.service"
+        ];
+        script = toString (
+          pkgs.writeShellScript "qca-bluetooth.sh" ''
+            set -x
+            trap 'sleep 1' DEBUG
+            export PATH="${
+              lib.makeBinPath (
+                with pkgs;
+                [
+                  config.hardware.bluetooth.package
+                  coreutils-full
+                  gawk
+                  unixtools.script
+                ]
+              )
+            }:$PATH"
 
-          SERIAL=$(grep -o "serialno.*" /proc/cmdline | cut -d" " -f1)
-          BT_MAC=$(echo "$SERIAL-BT" | sha256sum | awk -v prefix=0200 '{printf("%s%010s\n", prefix, $1)}')
-          BT_MAC=$(echo "$BT_MAC" | cut -c1-12 | sed 's/\(..\)/\1:/g' | sed '$s/:$//')
+            SERIAL=$(grep -o "serialno.*" /proc/cmdline | cut -d" " -f1)
+            BT_MAC=$(echo "$SERIAL-BT" | sha256sum | awk -v prefix=0200 '{printf("%s%010s\n", prefix, $1)}')
+            BT_MAC=$(echo "$BT_MAC" | cut -c1-12 | sed 's/\(..\)/\1:/g' | sed '$s/:$//')
 
-          script -qc "btmgmt --timeout 3 -i hci0 power off"
-          script -qc "btmgmt --timeout 3 -i hci0 public-addr \"$BT_MAC\""
-        ''
-      );
-      serviceConfig = {
-        User = "root";
-        Type = "oneshot";
-        RemainAfterExit = true;
+            script -qc "btmgmt --timeout 3 -i hci0 power off"
+            script -qc "btmgmt --timeout 3 -i hci0 public-addr \"$BT_MAC\""
+          ''
+        );
+        serviceConfig = {
+          User = "root";
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
       };
     };
-  };
 }
