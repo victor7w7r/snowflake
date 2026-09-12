1f6564138a837e1b3a5291a4e13ecce99722c579
victor7w7r <arkano036@gmail.com>
2026-09-12 09:59:06 -0500

    set q6 as meson

--- pkgs/by-name/q6voiced/package.nix
+++ pkgs/by-name/q6voiced/package.nix
@@ -6,18 +6,20 @@
 cache-stdenv.mkDerivation {
   pname = "q6voiced";
   version = "0.2.1";
+
   src = inputs.q6voiced;
 
   buildInputs = with pkgs; [
     alsa-lib
     dbus
+    glib
   ];
 
   nativeBuildInputs = with pkgs; [
     pkg-config
+    meson
+    ninja
   ];
 
-  makeFlags = [
-    "PREFIX=$(out)"
-  ];
+  configurePhase = "meson setup build --prefix=$out";
 }
