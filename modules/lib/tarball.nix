603d9c57254c2a869619afe698386a73581c1d8e
victor7w7r <arkano036@gmail.com>
2026-09-14 19:20:49 -0500

    set tarball in setup

--- modules/lib/tarball.nix
+++ modules/lib/tarball.nix
@@ -49,6 +49,9 @@
                 [
                   zstd
                   rsyncy
+                  gnutar
+                  zstd
+                  coreutils
                 ]
                 ++ additionalBuildInputs;
 
@@ -56,6 +59,87 @@
                 (pkgs.buildPackages.closureInfo { rootPaths = [ config.system.build.toplevel ]; })
                 |> (closureInfo: ''
                   mkdir -p $out root/store
+
+                  ${
+                    if enableGenericExtlinux then
+                      ''
+                        ${config.boot.loader.generic-extlinux-compatible.populateCmd} \
+                         -c ${config.system.build.toplevel} -d firmware/boot
+
+                        mv firmware/boot ./boot
+                        tar -cv -C boot . | zstd -T$NIX_BUILD_CORES > $out/boot.tar.zst
+                      ''
+                    else
+                      ''
+                        toplevel=${config.system.build.toplevel}
+                        dtbpath=${config.hardware.deviceTree.name}
+                        kernel_params=$(cat "$toplevel/kernel-params")
+                        esp="staging"
+
+                        mkdir -p $esp/EFI/BOOT $esp/EFI/systemd
+                        cp ${lib.getLib pkgs.systemd}/lib/systemd/boot/efi/systemd-boot*.efi $esp/EFI/systemd
+                        cp ${pkgs.systemd}/lib/systemd/boot/efi/* $esp/EFI/BOOT
+                        booter=$(basename $(ls $esp/EFI/BOOT/systemd-*.efi))
+                        mv $esp/EFI/BOOT/$booter $esp/EFI/BOOT/''${booter/systemd-/}
+
+                        kernel=$(readlink "$toplevel/kernel")
+                        kernel_name="''${kernel/\/nix\/store\//}"
+                        kernel_name="''${kernel_name/\//-}"
+                        efi_kernel="/EFI/nixos/$kernel_name.efi"
+                        install -Dm644 "$kernel" "$esp/$efi_kernel"
+
+                        initrd=$(readlink "$toplevel/initrd")
+                        initrd_name="''${initrd/\/nix\/store\//}"
+                        initrd_name="''${initrd_name/\//-}"
+                        efi_initrd="/EFI/nixos/$initrd_name.efi"
+                        install -Dm644 "$initrd" "$esp/$efi_initrd"
+
+                        efi_dtb=
+                        if [ -n "$dtbpath" ]; then
+                          dtbs=$(readlink "$toplevel/dtbs")
+                          dtbs_name="''${dtbs/\/nix\/store\//}"
+                          dtbs_name="''${dtbs_name/\//-}"
+                          efi_dtb="EFI/nixos/$dtbs_name-$(basename $dtbpath).efi"
+                          install -Dm644 "$dtbs/$dtbpath" "$esp/$efi_dtb"
+                        fi
+
+                        install -Dm644 "${pkgs.edk2-uefi-shell}/shell.efi" "$esp/EFI/shell.efi"
+                        install -Dm644 "${pkgs.writeText "poweroff.nsh" "reset -s"}" "$esp/EFI/poweroff.nsh"
+                        install -Dm644 "${pkgs.writeText "reboot.nsh" "reset -c"}" "$esp/EFI/reboot.nsh"
+
+                        mkdir -p $esp/loader/entries
+
+                        cat > $esp/loader/entries/nixos-generation-0.conf <<EOF
+                          title NixOS
+                          sort-key nixos
+                          version Generation 0 NixOS
+                          linux $efi_kernel
+                          initrd $efi_initrd
+                          options init=$toplevel/init $kernel_params
+                          ''${efi_dtb:+devicetree $efi_dtb}
+                        EOF
+
+                        cat > $esp/loader/entries/poweroff.conf <<EOF
+                          title Apagar (Poweroff)
+                         efi /EFI/shell.efi
+                          options -e -noexit /EFI/poweroff.nsh
+                        EOF
+
+                        cat > $esp/loader/entries/reboot.conf <<EOF
+                          title Reiniciar (Reboot)
+                          efi /EFI/shell.efi
+                          options -e -noexit /EFI/reboot.nsh
+                        EOF
+
+                        cat > $esp/loader/loader.conf <<EOF
+                          timeout 5
+                          default nixos-generation-0.conf
+                          console-mode keep
+                        EOF
+
+                        tar -cv -C $esp . | zstd -T$NIX_BUILD_CORES > $out/boot.tar.zst
+                      ''
+                  }
 
                   ${if additionalContent != "" then additionalContent else ""}
 
@@ -65,14 +149,6 @@
                   mv root/store/nix/store/* root/store/ && rm -rf root/store/nix
                   chmod -R a-w root/store
                   tar --owner=0 --group=0 --numeric-owner -cv -C root . | zstd -T$NIX_BUILD_CORES > $out/store.tar.zst
-
-                  ${lib.optionalString enableGenericExtlinux ''
-                    ${config.boot.loader.generic-extlinux-compatible.populateCmd} \
-                     -c ${config.system.build.toplevel} -d firmware/boot
-
-                    mv firmware/boot ./boot
-                    tar -cv -C boot . | zstd -T$NIX_BUILD_CORES > $out/boot.tar.zst
-                  ''}
                 '');
             };
           };
